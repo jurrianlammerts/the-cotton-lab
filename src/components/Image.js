c5837cb3c54686ce243b139348febbe0c7a4f0ce
Jurrian <jurrianlammerts@hotmail.com>
2020-03-11 20:51:01 +0100

    Removed env url from image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,7 @@
 import React from "react"
 
 const Image = ({ src, ...props }) => {
-  return src ? (
-    <img src={`${process.env.GATSBY_API_URL}${src}`} {...props} />
-  ) : null
+  return src ? <img src={src} {...props} /> : null
 }
 
 export default Image
